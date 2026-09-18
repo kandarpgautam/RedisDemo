aacf4768fb6d1a458146ff43f0a3e1c232dc9773
Kandarp Gautam <kandarp.gautam@tothenew.com>
2026-09-18 17:12:38 +0530

    Add project files.

---
+++ AppRunner.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Diagnostics;
+
+public class AppRunner
+{
+    private readonly IDistributedCache _cache;
+    public AppRunner(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+    public async Task RunAsync()
+    {
+        string cacheKey = "myKey";
+        string cacheValue = "Hello, Redis!";
+
+        // Absolute Expiration Relative To Now overrides Absolute Expiration if both are set.
+        var cacheConfig = new DistributedCacheEntryOptions() { AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(10), AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(15) };
+
+        Console.WriteLine($"Current Datetime: {DateTimeOffset.Now}.");
+        Console.WriteLine($"Absolute Expiration Relative To Now: {cacheConfig.AbsoluteExpirationRelativeToNow}.");
+        Console.WriteLine($"Absolute Expiration: {cacheConfig.AbsoluteExpiration}");
+        await _cache.SetStringAsync(cacheKey, cacheValue, cacheConfig);
+
+        
+        foreach (var i in Enumerable.Range(0, 20))
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string retrievedValue = await _cache.GetStringAsync(cacheKey) ?? "";
+            Console.WriteLine($"{i+1}. Retrieved value from cache: {retrievedValue}. Elapsed Time: {stopwatch.Elapsed.Milliseconds.ToString($"{0}ms")}");
+            Task.Delay(1000).Wait();
+            stopwatch.Stop();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var appSettings = File.ReadAllBytes("appsettings.json");
+
+IServiceCollection services = new ServiceCollection()
+    .AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = configuration.GetSection("RedisEndpoint").Value;
+        options.InstanceName = "RedisDemo";
+    }).AddTransient<AppRunner>();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+var app = serviceProvider.GetRequiredService<AppRunner>();
+await app.RunAsync();
